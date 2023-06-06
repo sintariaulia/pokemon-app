e6e9eaa1b060d20acf5c51d349336f0eed2642d8
sintariaulia <sintariaulia04@gmail.com>
2023-06-06 09:19:02 +0700

    fix: header and user

--- src/services/users/index.jsx
+++ src/services/users/index.jsx
@@ -15,11 +15,11 @@ const IndexUser = () => {
 
     const arrUser = user.map((data) => {
         return (
-            <div key={data.id} className='flex flex-col items-center'>
+            <div key={data.id} className='flex flex-col text-slate-700 items-center'>
                 <p className='pb-1 font-semibold' >
                     {data?.first_name} {data?.last_name}
                 </p>
-                <img alt='' key={data?.avatar} src={data?.avatar} />
+                <img alt='' key={data?.avatar} src={data?.avatar} className='rounded-full' />
                 <p className='pt-2'>{data?.email}</p>
             </div>
 
@@ -33,7 +33,7 @@ const IndexUser = () => {
                 <div className='bg-white/30  rounded-3xl max-w-md mx-auto  sm:max-w-lg md:max-w-3xl lg:max-w-5xl'>
                     <h1 className='text-2xl md:text-3xl lg:text-4xl font-extrabold text-center text-slate-600 py-8 lg:py-10'>Users</h1>
                     <div >
-                        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 pb-12 lg:pb-24 px-10 xl:px-20'>
+                        <div className='text-slate-600  grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 gap-10 pb-12 lg:pb-24 px-10 xl:px-20'>
                             {arrUser}
                         </div>
                     </div>
